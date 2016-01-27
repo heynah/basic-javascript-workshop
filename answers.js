1fa88a9d46ebc490de7b0e9ccfacbd7cf9eee7a1
heynah <hannanicol@gmail.com>
2016-01-27 19:28:06 +0000

    exercise one

--- answers.js
+++ answers.js
@@ -1,1 +1,14 @@
-//begining of basic-js-workshop
+//Write a function that takes a string and returns the first character of the string. 
+//Test your function on a few inputs, including the empty string.
+/*var firstLetter = function(string){
+    return string.substr(0,1);
+}
+
+console.log (firstLetter("String"));
+console.log (firstLetter("Object"));
+console.log (firstLetter(""));
+console.log (firstLetter("While"));
+console.log (firstLetter("HTML"));
+console.log (firstLetter("Array"));
+console.log (firstLetter("This"));
+console.log (firstLetter("?"));*/
